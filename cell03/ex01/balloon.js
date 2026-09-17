5112b091378692978cae3f110d7c0bc2770f7bb4
Sitthan181 <68070181@kmitl.ac.th>
2026-09-17 21:45:19 +0700

    add cell03 ex01

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,30 @@
+const balloon = document.getElementById("balloon");
+
+const colors = ["red", "green", "blue"];
+let colorIndex = 0;
+let size = 200;
+
+function updateBalloon() {
+    balloon.style.width = size + "px";
+    balloon.style.height = size + "px";
+    balloon.style.backgroundColor = colors[colorIndex];
+}
+
+balloon.addEventListener("click", function() {
+    if (size > 420) {
+        size = 200;
+    } else {
+        size += 10;
+        colorIndex = (colorIndex + 1) % colors.length;
+    }
+    updateBalloon();
+});
+
+balloon.addEventListener("mouseleave", function() {
+    if (size > 200) {
+        size -= 5;
+    }
+
+    colorIndex = (colorIndex - 1 + colors.length) % colors.length;
+    updateBalloon();
+});
